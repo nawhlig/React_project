683f2f95f2c7b6bd4e007ff7f49d7944c0f451c5
nawhlig <dksskd84@gmail.com>
2020-10-05 05:34:27 +0900

    삭제기능 제작중

---
+++ webui/src/Helper/Todo_list_delete.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import API from './Api';
+
+// ================ 리스트 삭제기능 ======================================================
+
+export default function Todo_list_delete({ seq, todolists, setTodolists }) {
+  //페이지 리로딩 상태변화
+  const [pageloading, setPageloading] = React.useState(false);
+  const [pageloadingerror, setPageloadingError] = React.useState(null);
+  console.log('delete 함수에서 받은 값', seq);
+
+  //삭제 로직
+  //   const deleteTodo = (seq) => {
+  //     API.delete("todo/todo/" + seq)
+  //     .then(res=>{
+
+  //         return API.get("todo/allTodo")
+  //                                        }).then(res=>{
+  //                                                       const {data} = res;
+  //                                                       setTodos(prev => data);
+  //         }
+  //     )
+  // }
+
+
+  //휴지통 버튼 클릭 이후 DB삭제, 페이지 재요청
+  const deleteTodo = (seq) => {
+    API.delete("todo/" + seq)
+      .then(response => {
+
+        const fetchTodolists = async () => {
+          try {// 요청이 시작 할 때에는 error 와 Todolist 를 초기화하고
+            setPageloadingError(null);
+            setTodolists(null);
+            setPageloading(true); // loading 상태를 true 로 바꿉니다.
+
+            const response = await API.get("todo/?status=할일");
+            await setTodolists(prev => ({ ...prev, pending: response.data }));
+
+            const response2 = await API.get("todo/?status=진행중");
+            await setTodolists(prev => ({ ...prev, inprogress: response2.data }));
+
+            const response3 = await API.get("todo/?status=종료");
+            await setTodolists(prev => ({ ...prev, end: response3.data }));
+            // form.resetFields()
+          }
+
+          catch (e) { setPageloadingError(e); } setPageloading(false);
+        }
+        fetchTodolists();
+
+        if (pageloading) return <div>로딩중..</div>;
+        if (pageloadingerror) return <div> 목록 불러오기 실패</div>;
+        if (!todolists) return null;
+      }
+      )
+  }
+  return (<></>);
+}
--- webui/src/pages/Todo_list.js
+++ webui/src/pages/Todo_list.js
@@ -2,21 +2,25 @@
 import React from 'react';
 import '../css/pages_subject.css';
 import styles from '../css/Todo_list.module.css';
-import { Button, List} from 'antd';
+import { Button, List } from 'antd';
 import { SearchOutlined, RestOutlined } from '@ant-design/icons';
 import API from '../Helper/Api';
-import Todo_list_form from '../Helper/Todo_list_form';
+import Todo_list_add from '../Helper/Todo_list_add';
+import Todo_list_delete from '../Helper/Todo_list_delete';
 
 export default function Page_todo_list() {
-    
+
     const [visible, setVisible] = React.useState(false);
     const [todolists, setTodolists] = React.useState(
-        { pending: [],
-          inprogress: [],
-          end: [] });
+        {
+            pending: [],
+            inprogress: [],
+            end: []
+        });
 
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(null);
+    const [seq, setSeq] = React.useState(0);
 
     React.useEffect(() => {
         const fetchTodolists = async () => {
@@ -26,13 +30,13 @@ export default function Page_todo_list() {
                 setLoading(true); // loading 상태를 true 로 바꿉니다.
                 // window.alert("목록 불러오는중");
                 const response = await API.get("todo/?status=할일");
-                await setTodolists(prev => ({...prev, pending:response.data}));
+                await setTodolists(prev => ({ ...prev, pending: response.data }));
 
                 const response2 = await API.get("todo/?status=진행중");
-                await setTodolists(prev => ({...prev, inprogress:response2.data}));
+                await setTodolists(prev => ({ ...prev, inprogress: response2.data }));
 
                 const response3 = await API.get("todo/?status=종료");
-                await setTodolists(prev => ({...prev, end:response2.data}));
+                await setTodolists(prev => ({ ...prev, end: response3.data }));
             }
             catch (e) { setError(e); }
             setLoading(false);
@@ -49,60 +53,65 @@ export default function Page_todo_list() {
 
             <div id="pagetop">
                 <div id="subject">할 일 목록</div>
-                <div id="listadd"><Button onClick={()=>{setVisible(true)}}> + 추가</Button></div>
+                <div id="listadd"><Button onClick={() => { setVisible(true) }}> + 추가</Button></div>
             </div>
             <div id="pagebottom">
                 <List
-                        header={<div>할일</div>}
-                        style={{width:"33%",float:"left",paddingLeft:"10px", paddingRight:"26px"}}
-                        itemLayout="horizontal"
-                        dataSource={todolists.pending}
-                        renderItem={item => (
-                            <List.Item>
-                                <List.Item.Meta title={<span>{item.name}</span>}
-                                                description={<span>{item.end_date}</span>}
-                                />
-                            <Button style={{float:"right"}} shape="circle" icon={<RestOutlined />} />                                
-                            </List.Item>
-                            
-                        )}
+                    header={<div>할일</div>}
+                    style={{ width: "33%", float: "left", paddingLeft: "10px", paddingRight: "26px" }}
+                    itemLayout="horizontal"
+                    dataSource={todolists.pending}
+                    renderItem={item => (
+                        <List.Item>
+                            <List.Item.Meta title={<span>{item.name}</span>}
+                                description={<span>{item.end_date}</span>}
+                            />
+                            <Button
+                                onMouseEnter={() => { setSeq(item.seq); console.log('리스트번호', item.seq); console.log('상태값', seq) }}
+                                onClick={() => { console.log('클릭한 seq값', seq); Todo_list_delete(seq); }}
+                                style={{ float: "right" }} shape="circle" icon={<RestOutlined />} />
+                        </List.Item>
+
+                    )}
                 />
 
                 <List
-                        header={<div>진행중</div>}
-                        style={{width:"33%",float:"left", paddingLeft:"18px", paddingRight:"18px"}}
-                        itemLayout="horizontal"
-                        dataSource={todolists.inprogress}
-                        renderItem={item => (
-                            <List.Item>
-                                <List.Item.Meta title={<span>{item.name}</span>}
-                                                description={<span>{item.end_date}</span>}
-                                />
-                            <Button style={{float:"right"}} shape="circle" icon={<RestOutlined />} />                            
-                            </List.Item>
-                        )}
+                    header={<div>진행중</div>}
+                    style={{ width: "33%", float: "left", paddingLeft: "18px", paddingRight: "18px" }}
+                    itemLayout="horizontal"
+                    dataSource={todolists.inprogress}
+                    renderItem={item => (
+                        <List.Item>
+                            <List.Item.Meta title={<span>{item.name}</span>}
+                                description={<span>{item.end_date}</span>}
+                            />
+                            <Button style={{ float: "right" }} shape="circle" icon={<RestOutlined />} />
+                        </List.Item>
+                    )}
                 />
 
                 <List
-                        header={<div>종료</div>}
-                        style={{width:"33%",float:"left",paddingLeft:"26px", paddingRight:"10px"}}
-                        itemLayout="horizontal"
-                        dataSource={todolists.end}
-                        renderItem={item => (
-                            <List.Item>
-                                <List.Item.Meta title={<span>{item.name}</span>}
-                                                description={<span>{item.end_date}</span>}
-                                />
-                            <Button style={{float:"right"}} shape="circle" icon={<RestOutlined />} />
-                            </List.Item>
-                        )}
+                    header={<div>종료</div>}
+                    style={{ width: "33%", float: "left", paddingLeft: "26px", paddingRight: "10px" }}
+                    itemLayout="horizontal"
+                    dataSource={todolists.end}
+                    renderItem={item => (
+                        <List.Item>
+                            <List.Item.Meta title={<span>{item.name}</span>}
+                                description={<span>{item.end_date}</span>}
+                            />
+                            <Button style={{ float: "right" }} shape="circle" icon={<RestOutlined />} />
+                        </List.Item>
+                    )}
                 />
-                <Todo_list_form visible={visible}
-                                setVisible={setVisible}
-                                todolists={todolists}
-                                setTodolists={setTodolists}
-
+                <Todo_list_add visible={visible}
+                    setVisible={setVisible}
+                    todolists={todolists}
+                    setTodolists={setTodolists}
                 />
+                <Todo_list_delete seq={seq}
+                    todolists={todolists}
+                    setTodolists={setTodolists} />
             </div>
 
         </>
