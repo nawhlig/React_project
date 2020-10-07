2fdcb5e48dc7cc53dbf23eb9c043673b8d28ca81
nawhlig <dksskd84@gmail.com>
2020-10-07 18:59:05 +0900

    Update Api.js

--- webui/src/Helper/Api.js
+++ webui/src/Helper/Api.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const API = Axios.create({
-    baseURL: "http://127.0.0.1:8000/drf/todo/",
+    //baseURL: "http://127.0.0.1:8000/drf/todo/",
+    baseURL: "http://ec2-3-35-3-10.ap-northeast-2.compute.amazonaws.com/drf/todo/",
     timeout: 3000,
 })
 export default API;
